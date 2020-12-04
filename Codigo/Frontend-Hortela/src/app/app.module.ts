39d6947d889eb870c9957d0b73148d9c2cb46dc9
Valney Faria <valneyfaria@hotmail.com>
2020-12-03 21:15:00 -0300

    Melhoria no Estilo da Tela de Login

--- Codigo/Frontend-Hortela/src/app/app.module.ts
+++ Codigo/Frontend-Hortela/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -18,6 +18,7 @@ import { CulturaComponent, MngCulturaDialog } from './cultura/cultura.component'
 import { Globals } from './globals/globals';
 import { HomeComponent } from './home/home.component';
 import { VendaComponent } from './venda/venda.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,9 @@ import { VendaComponent } from './venda/venda.component';
     MatInputModule,
     FormsModule,
     HttpClientModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   entryComponents: [
     MngCulturaDialog
